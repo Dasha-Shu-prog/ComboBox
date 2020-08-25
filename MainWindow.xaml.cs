060adf0e87dbeb406e41d45d0b2cfd4f795e6bae
Dasha <dashashulipa@gmail.com>
2020-08-25 23:46:13 +0000

    Рабочая без удаления строб из графика

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Shapes;
 using Key = System.Windows.Input.Key;
 using InteractiveDataDisplay.WPF;
 using System.Windows.Media.Effects;
+using System.Collections;
+using System.Windows.Controls.Primitives;
 
 namespace Table1
 {
@@ -22,97 +24,73 @@ namespace Table1
             private int val_ampl = 0;
             const int MIN_TIME = 0;
             const int MAX_TIME = 180;
-            const int MIN_AMPL = -20;
-            const int MAX_AMPL = 20;
-            List<int> xCoords = new List<int>();
-            List<int> yCoords = new List<int>();
+            const int MIN_AMPL = -15;
+            const int MAX_AMPL = 15;
             List<Strobe_Characteristic> listStrobe = new List<Strobe_Characteristic>();
-            Strobe_Characteristic strobe1;
+
         public MainWindow()
         {
             InitializeComponent();
             var y = Enumerable.Range(0, 2000).Select(i => i / 10.0).ToArray();
-            var x = y.Select(v => Math.Sin(v + 100) / 1.0).ToArray();
+            var x = y.Select(v => Math.Sin(v + 100) * 10).ToArray();
             var lg = new LineGraph();
             lines.Children.Add(lg);
             lg.Stroke = new SolidColorBrush(Colors.Black);
             lg.Plot(x, y);
-        }
+        }        
         private void Time_start_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (Int32.TryParse(time_start.Text, out int result_t_start))
-            {
-                ValidateData(ref result_t_start);
-                t_start = result_t_start;
-            }
-             time_start.Text = t_start.ToString();
-            if (t_start == MIN_TIME)
-                decrement_t_start.IsEnabled = false;
-
-            else if (t_start == MAX_TIME)
-                increment_t_start.IsEnabled = false;
-
-            else
-            {
-                decrement_t_start.IsEnabled = true;
-                increment_t_start.IsEnabled = true;
-            }
+            TextChanged(time_start, ref t_start, increment_t_start, decrement_t_start);
         }
         private void Time_stop_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (Int32.TryParse(time_stop.Text, out int result_t_stop))
-            {
-                ValidateData(ref result_t_stop);
-                t_stop = result_t_stop;
-            }
-             time_stop.Text = t_stop.ToString();
-            if (t_stop == MIN_TIME)
-                decrement_t_stop.IsEnabled = false;
-
-            else if (t_start == MAX_TIME)
-                increment_t_stop.IsEnabled = false;
-
-            else
-            {
-                decrement_t_stop.IsEnabled = true;
-                increment_t_stop.IsEnabled = true;
-            }
+        {          
+            TextChanged(time_stop, ref t_stop, increment_t_stop, decrement_t_stop);
         }
         private void Ampl_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (Int32.TryParse(ampl.Text, out int result_ampl))
+            TextChanged(ampl, ref val_ampl, increment_ampl, decrement_ampl, false);
+        }
+        private void TextChanged(TextBox textBox, ref int inputValue, RepeatButton incrementButton, RepeatButton decrementButton, bool isTime = true)
+        {
+            int minValue = isTime ? MIN_TIME : MIN_AMPL;
+            int maxValue = isTime ? MAX_TIME : MAX_AMPL;
+
+            if (Int32.TryParse(textBox.Text, out int result))
             {
-                ValidateDataAmpl(ref result_ampl);
-                val_ampl = result_ampl;
+                if (isTime)
+                    ValidateTime(ref result);
+                else 
+                    ValidateAmpl(ref result);
+
+                inputValue = result;
             }
-              ampl.Text = val_ampl.ToString();
-            if (val_ampl == MIN_AMPL)
-                decrement_ampl.IsEnabled = false;
-
-            else if (val_ampl == MAX_AMPL)
-                increment_ampl.IsEnabled = false;
-
+            textBox.Text = inputValue.ToString();
+            if (inputValue == minValue)
+                decrementButton.IsEnabled = false;
+            else if (inputValue == maxValue)
+                incrementButton.IsEnabled = false;
             else
             {
-                decrement_ampl.IsEnabled = true;
-                increment_ampl.IsEnabled = true;
+                decrementButton.IsEnabled = true;
+                incrementButton.IsEnabled = true;
             }
-        }
-        private void ValidateData(ref int value)
+        }       
+        private void ValidateTime(ref int value)
         {
-            if (value > MAX_TIME)
-                value = MAX_TIME;
-            if (value < MIN_TIME)
-                value = MIN_TIME;
-            //if (t_start > t_stop)
-               // MessageBox.Show("Время начала должно быть меньше времени конца!");
+            ValidateData(ref value);
         }   
-        private void ValidateDataAmpl(ref int value)
+        private void ValidateAmpl(ref int value)
         {
-            if (value > MAX_AMPL)
-                value = MAX_AMPL;
-            if (value < MIN_AMPL)
-                value = MIN_AMPL;
+            ValidateData(ref value, false);
+        }
+        private void ValidateData(ref int value, bool isTime = true)
+        {
+            int minValidValue = isTime ? MIN_TIME : MIN_AMPL;
+            int maxValidValue = isTime ? MAX_TIME : MAX_AMPL;
+            if (value > maxValidValue)
+                value = maxValidValue;
+            if (value < minValidValue)
+                value = minValidValue;
         }
         //private int num = 1;
         private void Table_LoadingRow(object sender, DataGridRowEventArgs e)
@@ -121,7 +99,9 @@ namespace Table1
         }
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
-            strobe1 = new Strobe_Characteristic 
+            if (t_start > t_stop)
+                MessageBox.Show("Время начала должно быть меньше времени конца!");
+            Strobe_Characteristic strobe = new Strobe_Characteristic 
             {
                 //Number = num,
                 Time_start = t_start,
@@ -129,22 +109,45 @@ namespace Table1
                 Amplitude = val_ampl,
                 Color = color_box.Text
             };
-            listStrobe.Add(strobe1);            
+            listStrobe.Add(strobe);            
             table.Items.Add(listStrobe.Last());
-            var linegraph = new LineGraph();
-            lines.Children.Add(linegraph);                           
-            linegraph.Stroke = new VisualBrush(color_box);
-            linegraph.Description = String.Format("Строба {0}", + 1);
-            linegraph.StrokeThickness = 2;
-            linegraph.Plot(strobe1.Time_stop, strobe1.Amplitude);
-            linegraph.Plot(strobe1.Time_start, strobe1.Amplitude);           
-            //Console.WriteLine(listStrobe);
+            var line = new LineGraph();
+            lines.Children.Add(line);
+            switch (strobe.Color)
+            {
+                case "Красный":
+                    line.Stroke = new SolidColorBrush(Colors.Firebrick);
+                    break;
+                case "Синий":
+                    line.Stroke = new SolidColorBrush(Colors.DarkBlue);
+                    break;
+                case "Зелёный":
+                    line.Stroke = new SolidColorBrush(Colors.DarkGreen);
+                    break;
+                case "Оранжевый":
+                    line.Stroke = new SolidColorBrush(Colors.DarkOrange);
+                    break;
+                case "Фиолетовый":
+                    line.Stroke = new SolidColorBrush(Colors.DarkMagenta);
+                    break;
+                case "Другой цвет":
+                    line.Stroke = new SolidColorBrush(Colors.BlueViolet);
+                    break;
+            }
+            List<int> xCoords = new List<int>();
+            List<int> yCoords = new List<int>();
+            line.StrokeThickness = 2;
+            xCoords.Add(strobe.Amplitude);
+            yCoords.Add(strobe.Time_start);
+            xCoords.Add(strobe.Amplitude);
+            yCoords.Add(strobe.Time_stop);
+            line.Plot(xCoords, yCoords);
         }
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
             table.Items.Remove(table.SelectedItem);            
             table.Items.Refresh();
-            listStrobe.Remove((Strobe_Characteristic)table.SelectedItem);                    
+            listStrobe.Remove((Strobe_Characteristic)table.SelectedItem);            
         }
         private void Time_start_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -160,39 +163,39 @@ namespace Table1
         }
         private void Decrement_t_start_Click(object sender, RoutedEventArgs e)
         {
-            int newTimeStart = --t_start;
-            ValidateData(ref newTimeStart);
-            time_start.Text = t_start.ToString();
+            DecrementClick(time_start, ref t_start);
         }
         private void Increment_t_start_Click(object sender, RoutedEventArgs e)
         {
-            int newTimeStart = ++t_start;
-            ValidateData(ref newTimeStart);
-            time_start.Text = t_start.ToString();
+            IncrementClick(time_start, ref t_start);
         }
         private void Decrement_t_stop_Click(object sender, RoutedEventArgs e)
         {
-            int newTimeStop = --t_stop;
-            ValidateData(ref newTimeStop);
-            time_stop.Text = t_stop.ToString();
+            DecrementClick(time_stop, ref t_stop);
         }
         private void Increment_t_stop_Click(object sender, RoutedEventArgs e)
         {
-            int newTimeStop = ++t_stop;
-            ValidateData(ref newTimeStop);
-            time_stop.Text = t_stop.ToString();
+            IncrementClick(time_stop, ref t_stop);
         }
         private void Decrement_ampl_Click(object sender, RoutedEventArgs e)
         {
-            int newAmplitude = --val_ampl;
-            ValidateData(ref newAmplitude);
-            ampl.Text = val_ampl.ToString();
+            DecrementClick(ampl, ref val_ampl);
         }
         private void Increment_ampl_Click(object sender, RoutedEventArgs e)
         {
-            int newAmplitude = ++val_ampl;
-            ValidateData(ref newAmplitude);
-            ampl.Text = val_ampl.ToString();
+            IncrementClick(ampl, ref val_ampl);
+        }
+        private void IncrementClick(TextBox textBox, ref int data)
+        {
+            int newValue = ++data;
+            ValidateTime(ref newValue);
+            textBox.Text = data.ToString();
+        }
+        private void DecrementClick(TextBox textBox, ref int data)
+        {
+            int newValue = --data;
+            ValidateTime(ref newValue);
+            textBox.Text = data.ToString();
         }
         private void Time_start_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
